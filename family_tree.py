e3aca80909cefaa4ce233cda1ee2f32a40543968
OussamaBouaziz <64372251+OussamaBouaziz@users.noreply.github.com>
2021-03-16 13:03:00 +0100

    final dictionary with siblings.. needs to be sorted

--- family_tree.py
+++ family_tree.py
@@ -18,13 +18,17 @@ def get_label_from_id(one_id):
 
 def looking_for_brothers_and_sisters(from_parents_table):
     bro_sis_labels = []
+    siblings_dictionary = {}
     with open("onto_x.csv") as csvfile:
         reader = csv.reader(csvfile)
         for row in reader:
             if from_parents_table[0] == row[2]:
                 bro_sis_labels.append(row[1])
 
-    return bro_sis_labels
+        for i in range(len(bro_sis_labels)):
+            siblings_dictionary.update({str(bro_sis_labels[i]): "0"})
+
+    return siblings_dictionary
 
 def all_grands(first_node,table_parents_labels):
 
@@ -50,7 +54,7 @@ def all_grands(first_node,table_parents_labels):
 
                     for k in range(len(grands)):
                         split_grands = grands[k].split("|") # The grands table doesn't have just one element.
-                        print("The direct-parents IDs are: ", split_grands)
+                        # print("The direct-parents IDs are: ", split_grands)
 
 
                     for j in range(len(split_grands)):
@@ -58,7 +62,7 @@ def all_grands(first_node,table_parents_labels):
                         grands_labels.append(str(label))
                         new_node = Node(str(grands_labels[j]), parent=node_parent)
 
-                        print(new_node.depth, "3oooooooooom9")
+                        #print(new_node.depth)
 
                         final_dictionary.update({str(grands_labels[j]): new_node.depth})
 
@@ -80,12 +84,15 @@ with open("onto_x.csv") as csvfile:
             parents.append(row[2])
             Adam = Node(str(row[1]))
 
-    print(parents,"333333333333333333333333")
+    # print(parents,"333333333333333333333333")
 
-    print(looking_for_brothers_and_sisters(parents), "brudiiiiis and schwestiiiiis !!!!")
+
 
 
 final_dictionary = {}
+# print(looking_for_brothers_and_sisters(parents))
+
+print("This is my final answer ", final_dictionary.update(looking_for_brothers_and_sisters(parents)))
 
 # print("The parents are: ", parents)
 split_parents = parents[0].split("|")
