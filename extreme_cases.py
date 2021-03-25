3b27576442204955d5ec2b88053d039ace6cc087
OussamaBouaziz <64372251+OussamaBouaziz@users.noreply.github.com>
2021-03-25 11:01:50 +0100

    it has been a while since I am trying to build the function get_line. this is not the result i am hoping for

--- extreme_cases.py
+++ extreme_cases.py
@@ -7,20 +7,23 @@ label_input = input()
 
 
 def get_line(string):   #to be applied for extreme cases null, www .. ,
-
+    ancestor = ""
     with open("onto_x.csv") as csvfile:
         reader = csv.reader(csvfile)
         i=1
 
         for row in reader:
             if string != row[1]:
-                i+=1
-            else: print("The label is in the ", i, "th line", row)
+                i += 1
+            else: break
+
+        print("The label is in the ", i, "th line", ancestor.join(row[1]))
 
 
-    return (i, label)
+        return (ancestor, i)
 
 get_line(label_input)
+print("The result is ", get_line(label_input)[0])
 
 
 
@@ -33,7 +36,7 @@ def extreme_or_not(string):
                 print("the label is", get_line(string)[1])
 
 
-extreme_or_not(label)
+#extreme_or_not(label)
 
 
 
