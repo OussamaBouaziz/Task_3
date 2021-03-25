988b5d927db26046e0b1a74d9ebbc75c048ce6ac
OussamaBouaziz <64372251+OussamaBouaziz@users.noreply.github.com>
2021-03-25 11:30:39 +0100

    that looks much better

--- extreme_cases.py
+++ extreme_cases.py
@@ -17,17 +17,15 @@ def get_line(string):   #to be applied for extreme cases null, www .. ,
             if string != row[1]:
                 i += 1
             else: break
-            ancestor = father.join(row[1])
+        ancestor = father.join(row[1])
+        print("The ancestor <<<<<<<<<<<<<<<<<<<<<", ancestor)
+        print("the whole row is", row)
         print("The label is in the ", i, "th line", ancestor)
-
-
 
     return (ancestor, i)
 
 get_line(label_input)
-print("The result is ", get_line(label_input)[0])
-
-
+# print("The result is ", get_line(label_input)[0])
 
 def extreme_or_not(string):
 
@@ -36,7 +34,6 @@ def extreme_or_not(string):
         for row in reader:
             if row[2] == ("http://www.w3.org/2002/07/owl#Thing" or "null"):
                 print("the label is", get_line(string)[1])
-
 
 #extreme_or_not(label)
 
