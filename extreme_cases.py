207f6e2dc5dd40ccb7412db1c60d4932d1c6d5fb
OussamaBouaziz <64372251+OussamaBouaziz@users.noreply.github.com>
2021-03-26 11:04:01 +0100

    the null problem is fixed

--- extreme_cases.py
+++ extreme_cases.py
@@ -18,9 +18,9 @@ def get_line(string):   #to be applied for extreme cases null, www .. ,
                 i += 1
             else: break
         ancestor = father.join(row[1])
-        print("The ancestor <<<<<<<<<<<<<<<<<<<<<", ancestor)
-        print("the whole row is", row)
-        print("The label is in the ", i, "th line", ancestor)
+        #print("The ancestor <<<<<<<<<<<<<<<<<<<<<", ancestor)
+        #print("the whole row is", row)
+        #print("The label is in the ", i, "th line", ancestor)
 
     return (ancestor, i, row[2])
 
@@ -29,8 +29,8 @@ def get_line(string):   #to be applied for extreme cases null, www .. ,
 
 def extreme_or_not(string):
 
-     if get_line(label_input)[2] == ("http://www.w3.org/2002/07/owl#Thing" or "null"):
-            print("the label is you should use is ", get_line(string)[1])
+     if get_line(label_input)[2] == "http://www.w3.org/2002/07/owl#Thing" or get_line(label_input)[2] in (None,""):
+            print("the label is you should use is ", get_line(string)[0])
 
 
 
