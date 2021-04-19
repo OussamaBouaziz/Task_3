899ed58553c403a74aa7abc46ae719f7aaf359e0
OussamaBouaziz <64372251+OussamaBouaziz@users.noreply.github.com>
2021-04-19 10:39:17 +0200

    all functions work

--- test.py
+++ test.py
@@ -5,6 +5,14 @@ import csv
 print("what label are you looking for ?")
 label_input = input()
 
+def get_label_from_id(one_id):
+    with open("onto_x.csv") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            if one_id == row[0]:
+                a_parents_label = row[1]
+                print(a_parents_label)
+
 
 def get_line(string):   #to be applied for extreme cases null, www .. ,
     father = ""
@@ -19,36 +27,51 @@ def get_line(string):   #to be applied for extreme cases null, www .. ,
 
             else: break
 
-    ancestor = father.join(row[1])
+    ancestor = father.join(row[2])
+
     print("The ancestor <<<<<<<<<<<<<<<<<<<<<", ancestor)
+
     print("the whole row is", row)
     print("The label is in the ", i, "th line", ancestor)
 
-    return (ancestor, i, row[2])
+    return (ancestor.split("|"), i, row[2])
+print("so this is ancestor", get_line(label_input)[0])
 
-# get_line(label_input)
+#get_line(label_input)
 # print("The result is ", get_line(label_input)[2])
 
 def extreme_or_not(string):
-
-     if get_line(label_input)[2] == "http://www.w3.org/2002/07/owl#Thing" or get_line(label_input)[2] in (None,""):
+    print("extreme cases now")
+    if get_line(label_input)[2] == "http://www.w3.org/2002/07/owl#Thing" or get_line(label_input)[2] in (None,""):
             print("the label is you should use is ", get_line(string)[0])
-     else : print(("the parents are ", get_line(string)[2]))
+    else : print(("the parents are ", get_line(string)[2]))
 
+def get_parents(table_child):
+    parents = []
+    generation = 0
+    all_of_them = []
+    with open("onto_x.csv") as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            for i in range(len(table_child)):
+                if table_child[i] == row[0]:
+                    # generation = generation + 1
+                    print(table_child, "if is visited for the ", i, "time")
+                    parents.append(row[2])  # Construire le tableau des parents [de la case i du table_child[i] ]
+                    split_parents = parents[0].split("|")
+                    print("split parents of ", get_label_from_id(table_child[i]), "are >>>>", split_parents,
+                          "this is the ", generation, "generation")
+                    # generation = generation + 1
+                    for j in range(len(split_parents)):
+                        generation = generation + 1
+                        # print("We are now in the for LOOOOOOP<<<<<<<<", j)
+                        # generation = generation + 1
+                        print(get_label_from_id(split_parents[j]), "This is one parent from the > ", generation,
+                              " < genration")
+                        get_parents(split_parents)
 
+    return (generation)
 
-
-
+get_line(label_input)[2]
+get_parents(get_line(label_input)[0])
 extreme_or_not(label_input)
-
-
-
-
-
-
-
-
-
-
-
-
